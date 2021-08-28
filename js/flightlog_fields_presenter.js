447730f6535e3455825b6a2024fedce59c1cdb54
Alexander van Saase <avsaase@gmail.com>
2021-08-28 22:23:52 +0200

    setpointRate[all] friendly name

--- js/flightlog_fields_presenter.js
+++ js/flightlog_fields_presenter.js
@@ -28,6 +28,7 @@ function FlightLogFieldPresenter() {
         'rcCommand[2]': 'rcCommand[yaw]',
         'rcCommand[3]': 'rcCommand[throttle]',
 
+        'axisRate[all]': 'setpointRate[all]',
         'axisRate[0]': 'setpointRate[roll]',
         'axisRate[1]': 'setpointRate[pitch]',
         'axisRate[2]': 'setpointRate[yaw]',
