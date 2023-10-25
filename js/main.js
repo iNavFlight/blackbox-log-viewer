7bab54016c8311980c97065eed28367e02bf7b5e
UAV Tech <spatzengr@yahoo.com>
2023-10-25 08:39:08 -0400

    Min. Zoom to 2%
    Changes min. zoom on main windows to 2% and increment to 2 to allow for a bigger look at the log for NAV moves troublehshooting and tuning.

--- js/main.js
+++ js/main.js
@@ -32,10 +32,10 @@ function BlackboxLogViewer() {
         PLAYBACK_MAX_RATE = 300,
         PLAYBACK_DEFAULT_RATE = 100,
         PLAYBACK_RATE_STEP = 5,
-        GRAPH_MIN_ZOOM = 10,
+        GRAPH_MIN_ZOOM = 2,
         GRAPH_MAX_ZOOM = 1000,
         GRAPH_DEFAULT_ZOOM = 100,
-        GRAPH_ZOOM_STEP = 10;
+        GRAPH_ZOOM_STEP = 2;
     
     var
         graphState = GRAPH_STATE_PAUSED,
